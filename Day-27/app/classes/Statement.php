d39552cb207e94551894da55bbe8a5219242d70a
Nuzmul Hossain Nahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-11-24 15:31:25 +0600

    foreach loop

--- Day-27/app/classes/Statement.php
+++ Day-27/app/classes/Statement.php
@@ -8,11 +8,42 @@ class Statement{
 
     public function forloop()
     {
-        $this->data = [10,20,30,"Nahid",123,true];
-        foreach($this->data as $item)
+        $this->data=[
+            
+            0=>[
+            'name'  =>'Nahid',
+            'mobile'=>'01212',
+            'email' =>'fghhgfjhg@gmail.com'
+            ],
+            1=>[
+            'name'  =>'Hasan',
+            'mobile'=>'01212',
+            'email' =>'dffdf@gmail.com'
+            ]
+        ];
+        
+
+        foreach($this->data as $key=> $item)
         {
-            echo $item." </br>";
+            foreach($item as $value)
+            {
+                echo $value." ";
+            }
+            echo'<br/>';
+            
         }
+        // foreach($this->data[1] as $key=> $item)
+        // {
+        //     echo $key.":".$item."</br>";
+        // }
+        // echo $this->data[1]['name'];
+        // echo$this->data['name'];
+        //for each
+        // $this->data = [10,20,30,"Nahid",123,true];
+        // foreach($this->data as $item)
+        // {
+        //     echo $item." </br>";
+        // }
         
 
         // //DO While
