9f700a7292d2d5147a9734f57acecbc7422d101d
Nuzmul Hossain Nahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-11-24 15:04:36 +0600

    Update Statement.php

--- Day-27/app/classes/Statement.php
+++ Day-27/app/classes/Statement.php
@@ -3,12 +3,35 @@
 namespace App\classes;
 
 class Statement{
-    public $name;
+    public $i;
+    public $data =[];
 
     public function forloop()
     {
-        for($this->i=0; $this->i <5; $this->i++){
-            echo'hrllow world</br>';
+        $this->data = [10,20,30,"Nahid",123,true];
+        foreach($this->data as $item)
+        {
+            echo $item." </br>";
         }
+        
+
+        // //DO While
+        // $this->i = 10;
+        // do{
+        //     echo $this->i."";
+        //     $this->i++;
+        // }
+        // while($this->i<20);
+        //while
+        // $this->i = 10;
+        // while($this->i <30)
+        // {
+        //     echo $this->i."";
+        //     $this->i++;
+        // }
+        // for
+        // for($this->i=0; $this->i <5; $this->i++){
+        //     echo'hrllow world</br>';
+    //}
     }
 }
