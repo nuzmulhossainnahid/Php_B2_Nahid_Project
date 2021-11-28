5cf886a21032f25316b95e10ef101fda19411849
Nuzmul Hossain Nahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-11-28 16:24:40 +0600

    Update Pattern.php

--- Day_29/app/classes/Pattern.php
+++ Day_29/app/classes/Pattern.php
@@ -4,14 +4,30 @@ namespace App\classes;
 
 class Pattern
 {
-        public $n;
+        public $n=5;
         public $i;
         public $j;
 
     public function patternOne()
     {
-        $this->n=4;
+       
         for ($this->i = 1; $this->i <=$this->n;  $this->i++)
+        {
+            for ($this->j = 1; $this->j <=($this->n*2)-1;  $this->j++)
+            {
+                if(($this->j>= $this->n - ($this->i -1)) && ($this->j<=$this->n + ($this->i-1)))
+                {
+                    echo '*';
+                }
+                else{
+                    echo '&nbsp;&nbsp;';
+                }
+            }
+            
+            echo'</br>';
+        }
+       
+        for ($this->i = $this->n-1; $this->i >=0;  $this->i--)
         {
             for ($this->j = 1; $this->j <=($this->n*2)-1;  $this->j++)
             {
