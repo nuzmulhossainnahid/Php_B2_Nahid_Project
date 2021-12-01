a35de079d43b6ba416792a3b97f95694b3f0a0ca
Nuzmul Hossain Nahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-12-01 17:08:04 +0600

    End

--- Day_32/app/classes/ImageUpload.php
+++ Day_32/app/classes/ImageUpload.php
@@ -11,6 +11,8 @@ class ImageUpload
     protected $imageName;
     protected $filetype;
     protected $check;
+    protected $name;
+    protected $tempName;
     
     protected $directory;
 //    public $result = [];
@@ -27,12 +29,15 @@ class ImageUpload
 //        print_r($this->file);
 ////        print_r($_FILES);
 //        echo '</pre>';
+
+        
+        
         
         $this->imageName= $this->file['image']['name'];
-        $this->directory = '../assets/img/upload/'.$this->imageName;
-        
         $this->filetype= pathinfo($this->imageName, PATHINFO_EXTENSION);
-        
+        $this->tempName = str_replace(' ', '_',$this->post['name']);
+        $this->name = $this->tempName.'_'.time().'.'.$this->filetype;
+        $this->directory = '../assets/img/upload/'.$this->name;
         $this->check = getimagesize($this->file['image']['tmp_name']);
         
         if ($this->check)
