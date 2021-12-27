f6dfbda659666708e50ffd056f3f79184426d4ba
Nuzmul Hossain Nahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2021-12-27 15:21:39 +0600

    Update master.blade.php

--- ecom/resources/views/admin/master.blade.php
+++ ecom/resources/views/admin/master.blade.php
@@ -63,7 +63,7 @@
                                 <a class="list-group-item">
                                     <div class="media">
                                         <div class="media-img">
-                                            <img src="./assets/img/users/u1.jpg" />
+                                            <img src="{{asset('/admin')}}/assets/img/users/u1.jpg" />
                                         </div>
                                         <div class="media-body">
                                             <div class="font-strong"> </div>Jeanne Gonzalez<small class="text-muted float-right">Just now</small>
@@ -161,14 +161,20 @@
                 </li>
                 <li class="dropdown dropdown-user">
                     <a class="nav-link dropdown-toggle link" data-toggle="dropdown">
-                        <img src="./assets/img/admin-avatar.png" />
+                        <img src="{{asset('/admin')}}/assets/img/admin-avatar.png" />
                         <span></span>Admin<i class="fa fa-angle-down m-l-5"></i></a>
                     <ul class="dropdown-menu dropdown-menu-right">
                         <a class="dropdown-item" href="profile.html"><i class="fa fa-user"></i>Profile</a>
                         <a class="dropdown-item" href="profile.html"><i class="fa fa-cog"></i>Settings</a>
                         <a class="dropdown-item" href="javascript:;"><i class="fa fa-support"></i>Support</a>
                         <li class="dropdown-divider"></li>
-                        <a class="dropdown-item" href="login.html"><i class="fa fa-power-off"></i>Logout</a>
+                        <a class="dropdown-item" href="" onclick="event.preventDefault();document.getElementById('logoutForm').submit()" >
+                            <i class="fa fa-power-off"></i>Logout
+                        </a>
+                        <form action="{{route('logout')}}" method='POST' id="logoutForm">
+                            @csrf
+
+                        </form>
                     </ul>
                 </li>
             </ul>
