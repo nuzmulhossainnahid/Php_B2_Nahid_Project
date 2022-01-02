5bb6ad4fe2c5690c4e1ed074776b5b2c980d831e
Nuzmul Hossain Nahid <60293729+nuzmulhossainnahid@users.noreply.github.com>
2022-01-02 14:19:25 +0600

    Update ecom.zip

--- ecom/resources/views/admin/includes/scripts.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Web App Develop-PHP
- * Date: 27-Dec-21
- * Time: 3:27 PM
- */
--- ecom/resources/views/front/about/about.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Mojahid Rafi
- * Date: 25/12/2021
- * Time: 1:11 PM
- */
